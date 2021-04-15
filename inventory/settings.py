a8c7164185982b98938d9d646d2917fc1ece61c7
unknown <mpilepic@student.uniri.hr>
2021-04-15 17:57:05 +0200

    update

--- inventory/settings.py
+++ inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k2z9po4i#n+1p(^ny1el2c!om(^-l+_%&ob0azk0-ike*-)81e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'iso-app.herokuapp.com']
 
 
 # Application definition
